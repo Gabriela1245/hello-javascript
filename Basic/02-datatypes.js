6f91d2476a001cb3380e7f27bf95194b72414ced
Gabriela Anjeli Herrera Almeyda <137328810+Gabriela1245@users.noreply.github.com>
2025-04-03 11:52:54 -0500

    Aclaramientos 02-datatypes.js

--- Basic/02-datatypes.js
+++ Basic/02-datatypes.js
@@ -14,15 +14,18 @@ let email = `braismoure@mouredev.com`
 let age = 37 // Entero
 let height = 1.77 // Decimal
 
-// Booleanos (boolean)
+// Booleanos (boolean). 
+// Solo se pueden tener 2 valores: True y false.
 let isTeacher = true
 let isStudent = false
 
 // Undefined
+//Para una variable uqe ha sido declarada, pero no inicializada, es decir, no se le ha asignado ningún valor.
 let undefinedValue
 console.log(undefinedValue)
 
 // Null
+//Sirve para declarar una variable con un valor, pero vale "nulo" porque ahora no se sabe que irá alli; quizas a futuro muy probablemente se le asigne un valor como tal .
 let nullValue = null
 
 // Symbol
@@ -30,11 +33,11 @@ let nullValue = null
 let mySymbol = Symbol("mysymbol")
 
 // BigInt
-
+//Para introducir grandes cnatidades de números enteros, ya que normalmente a un número se le da un almacenamiento de 64 bits. Pero en este caso con BigInt el limite de memoria es el del almacenamiento de tu pc.
 let myBigInt = BigInt(817239871289371986589716389471628379612983761289376129)
 let myBigInt2 = 817239871289371986589716389471628379612983761289376129n
 
-// Mostramos los tipos de datos
+// Mostramos los tipos de datos, muestra qué tipo de dato es: Un string, número, etc 
 console.log(typeof myName)
 console.log(typeof alias)
 console.log(typeof email)
